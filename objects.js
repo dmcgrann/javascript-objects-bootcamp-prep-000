7b513c1eb86c07f0035d7cded1f924c389ac46b0
David McGrann <dpmcgrann@gmail.com>
2017-12-27 20:36:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete (playlist, { Kayne: 'Gold Digger' })
-  return playlist
+  return ({}, playlist, { 'Kanye': "Gold Digger" })
 }
