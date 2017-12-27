2e706b47ab477aad4246e7a989931b37582dcb67
David McGrann <dpmcgrann@gmail.com>
2017-12-27 20:03:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,8 @@ var playlist = { artistName: 'songTitle' }
 function updatePlaylist(playlist, artistName, songTitle) {
   return ({}, playlist, { 'Phil Ochs': "Here\'s to the State of Mississippi" })
 }
+
+function removeFromPlaylsit(playlist, artistName, songTitle) {
+  delete { artistName: 'songTitle' }
+  return ({}, playlist, { artistName: 'songTitle'})
+}
