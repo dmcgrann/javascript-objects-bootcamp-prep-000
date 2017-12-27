498e5efc6963ea2c0f7e20d39904f68b5e2e9170
David McGrann <dpmcgrann@gmail.com>
2017-12-27 19:46:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { artistName: 'songTitle' }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return playlist
+  return Object.assign(playlist, 'Phil Ochs', 'Here/s to the State of Mississippi')
 }
